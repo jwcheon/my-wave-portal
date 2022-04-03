875ae00c659810600d73fa48f46a8299fb280431
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-04-03 21:30:38 +0900

    End of Section 2

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/_wQmznnalKZum2eOzT8svA9A4r4jGfZI",
+      accounts: ["4904b6bf671450d9700b608b9ea5a9950ba859d17024ef8b165d171c7f04d608"]
+    },
+  },
+};
